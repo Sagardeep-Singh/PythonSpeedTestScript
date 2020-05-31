158cd9bac2d990d06bbdf12ef0eb3a93966dd1a2
Sagardeep Singh <Sagardeep.singh@gmail.com>
2020-05-31 04:44:56 -0700

    Changed the position of the insert query declaration

--- script.py
+++ script.py
@@ -5,6 +5,14 @@ from functions import *
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+
+insert_query = """INSERT INTO
+                    speed_log(upload_speed,download_speed,latency,ip,isp,url)
+                VALUES
+                    (%s,%s,%s,%s,%s,%s)
+"""
+
+
 connection = db.db(
     host=config['DATABASE']['HOST'],
     username=config['DATABASE']['USERNAME'],
@@ -12,11 +20,6 @@ connection = db.db(
     database=config['DATABASE']['DB_NAME']
 )
 
-insert_query = """INSERT INTO
-                    speed_log(upload_speed,download_speed,latency,ip,isp,url)
-                VALUES
-                    (%s,%s,%s,%s,%s,%s)
-"""
 results = testSpeed()
 connection.execute(query=insert_query, bindings=(
     round(results['upload']/(10 ** 6), 2), round(results['download']/(10 ** 6), 2), round(results['ping'], 2), results['client']['ip'], results['client']['isp'], results['share']))
